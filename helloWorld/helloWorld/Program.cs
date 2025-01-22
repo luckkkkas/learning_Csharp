f03289c80636323265a706e41fa02fed10fd08f6
lucas oliveira <lucashbo321@gmail.com>
2025-01-22 20:35:37 -0300

    first commit

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using helloWorld;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string frase = Console.ReadLine();
+
+            // string[] fruta = frase.Split(',');
+            // for (int i = 0; i < fruta.Length; i++) {
+            ///   Console.WriteLine(fruta[i]); 
+
+            // }
+
+            //Console.WriteLine("entre com seu nome completo:");
+            //string nome = Console.ReadLine();
+            //Console.WriteLine("quantos quartos tem a sua casa?");
+            //int quartos = int.Parse(Console.ReadLine());
+            //Console.WriteLine("preço de um produto?");
+            //int preco = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("nome, idade, altura? (mesma linha)");
+            //string nomes = Console.ReadLine();
+
+
+            //string[] numbre = nomes.Split(' ');
+
+            //Console.WriteLine(nomes);
+            //Console.WriteLine(quartos);
+            //Console.WriteLine(preco);
+
+            //for (int i = 0; i < nomes.Length; i++) {
+            //    Console.WriteLine(numbre[i]);
+            //}
+
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("entre a largura e altura do triangulo:");
+            string dadosRecebidos = Console.ReadLine();
+            retangulo.Altura = float.Parse(dadosRecebidos.Split(' ')[0]);
+            retangulo.Largura = float.Parse(dadosRecebidos.Split(' ')[1]);
+
+            Console.WriteLine($"Area {retangulo.Area()}");
+            Console.WriteLine($"Perímetro {retangulo.Perimetro()}");
+            Console.WriteLine($"Hipotenusa {retangulo.Hipotenusa()}");
+        }
+         
+    }
+}
---
+++ helloWorld/helloWorld/Retangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld {
+    internal class Retangulo {
+        public float Altura;
+        public float Largura;
+
+        public float Area() {
+            return Largura * Altura;
+        }
+
+        public float Perimetro() {
+            return (Largura * 2) + (Altura * 2);
+        }
+
+        public double Hipotenusa() {
+            double catetoA = Math.Pow(Largura,2);
+            double catetoC = Math.Pow(Altura, 2);
+            double hipotenusa = Math.Sqrt(catetoA + catetoC);
+            return hipotenusa;
+        }
+    }
+}
