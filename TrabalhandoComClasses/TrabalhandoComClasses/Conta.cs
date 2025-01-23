d6371b7279fed6bf3de019cf9bef04e0602d20ac
lucas oliveira <lucashbo321@gmail.com>
2025-01-23 16:37:20 -0300

    trabalhando com classes em c#

---
+++ TrabalhandoComClasses/TrabalhandoComClasses/Conta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComClasses {
+    internal class Conta {
+
+        public float Saldo { get; private set; }
+        public string nome { get; private set; }
+        public int Nconta { get; private set; }
+        
+        public Conta(int nconta, string nome, float depInicial) {
+            this.Saldo = depInicial;
+            this.nome = nome;
+            this.Nconta = nconta;
+        }
+
+        public float sacar(float saque) {
+            int taxa = 5;
+            this.Saldo -= (saque + taxa);
+            return this.Saldo;
+        }
+
+        public float depositar(float dep) {
+            return this.Saldo += dep;
+        }
+    }
+}
---
+++ TrabalhandoComClasses/TrabalhandoComClasses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using TrabalhandoComClasses;
+
+namespace MyApp {
+    internal class Program {
+        static void Main(string[] args) {
+            float depInicial = 0;
+            Console.WriteLine("Entre com número da conta:");
+            int nConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Vai haver depósito inicial[s/n]?");
+            string depInicialSN = Console.ReadLine();
+
+            if (depInicialSN.ToLower() == "s") {
+                Console.WriteLine("Informe o valor inicial");
+                depInicial = float.Parse(Console.ReadLine());
+            }
+            Conta conta1 = new Conta(nConta, nome, depInicial);
+
+
+            Console.WriteLine($"Conta {conta1.Nconta}, titular {conta1.nome}, saldo {conta1.Saldo}");
+
+            Console.WriteLine("entre um valor para depósito:");
+                float dep = float.Parse(Console.ReadLine());
+            conta1.depositar(dep);
+            Console.WriteLine($"Conta {conta1.Nconta}, titular {conta1.nome}, saldo {conta1.Saldo}");
+
+            Console.WriteLine("entre um valor para sacar:");
+                float saq = float.Parse(Console.ReadLine());
+            conta1.sacar(saq);
+            Console.WriteLine($"Conta {conta1.Nconta}, titular {conta1.nome}, saldo {conta1.Saldo}");
+        }
+    }
+}
