315454461d241d20729dd98af9b66fab88b47e33
lucas oliveira <lucashbo321@gmail.com>
2025-02-26 16:09:29 -0300

    add exceptions

---
+++ Excecoes/Excecoes/Entities/Excepion/DomainException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Excecoes.Entities.Excepion {
+    internal class DomainException : ApplicationException{
+        public DomainException(string message) : base(message) { }
+    }
+}
