9a0c481b8995864ecd3e97bcd584a57151de3d7d
lucas oliveira <lucashbo321@gmail.com>
2025-02-26 21:21:13 -0300

    Exercicios

---
+++ Read_logFiles/Read_logFiles/Entities/LogRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_logFiles.Entities {
+    internal class LogRecord {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string username, DateTime instant) {
+            Username = username;
+            Instant = instant;
+        }
+
+        public override bool Equals(object? obj) {
+            if (!(obj is LogRecord)) return false;
+
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+
+        public override int GetHashCode() {
+            return Username.GetHashCode();
+        }
+    }
+}
---
+++ Read_logFiles/Read_logFiles/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Read_logFiles.Entities;
+
+Console.Write("Insira o camingo do arquivo de Log:");
+string logPath = Console.ReadLine();
+HashSet<LogRecord> list = new HashSet<LogRecord>();
+
+try {
+    using (StreamReader sr = File.OpenText(logPath)) {
+        while (!sr.EndOfStream) {
+            string line = sr.ReadLine();
+            string[] rows = line.Split(' ');
+            DateTime instant = DateTime.Parse(rows[1]);
+            list.Add(new LogRecord(rows[0].ToString(), instant));
+        }
+        Console.WriteLine($"{list.Count()} usuários Acessaram o site;");
+    }
+} catch (IOException err) {
+    Console.WriteLine(err.Message);
+}
