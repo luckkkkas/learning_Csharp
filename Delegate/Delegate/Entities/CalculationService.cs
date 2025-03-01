0176703a98dbd58f719f00a3c505ce43c1857e73
lucas oliveira <lucashbo321@gmail.com>
2025-03-01 17:49:43 -0300

    Delegate

---
+++ Delegate/Delegate/Entities/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate.Entities {
+    internal class CalculationService {
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+        public static int square(int a) {
+            return a * a;
+        }
+        public static int max(int a, int b) {
+            return (a > b) ? a : b;
+        }
+
+        public static void Soma(int a, int b) {
+            Console.WriteLine(a + b);
+        }
+        public static void ExibeMax(int a, int b) {
+            Console.WriteLine((a > b) ? a : b);
+        }
+    }
+}
---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Delegate.Entities;
+class Program {
+    // delegate int BinaryOptions(int n1, int n2);
+    delegate void BinaryOptions(int n1, int n2);
+    static void Main() {
+        int a = 1;
+        int b = 3;
+
+        //BinaryOptions op = CalculationService.sum; //precisa colocar o STATIC na função para funcionar//////////
+        //int result = op.Invoke(a, b);
+        //Console.WriteLine(result);
+
+        BinaryOptions op = CalculationService.ExibeMax;
+        op += CalculationService.Soma;
+
+        op.Invoke(a, b);
+        //ou 
+        op(a, b);
+    }
+}
