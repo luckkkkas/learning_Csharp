a0d60abc8856520e992f6afe16759c83b5c661b8
lucas oliveira <lucashbo321@gmail.com>
2025-03-03 17:41:12 -0300

    linq Exercicios

---
+++ Exercicio_Linq/Exercicio_Linq/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Exercicio_Linq.Entities {
+    internal class Product: IComparable<Product> {
+        public string name {  get; set; }
+        public double price { get; set; }
+
+        public Product(string name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public override string ToString() {
+            return name;
+        }
+
+        public int CompareTo(Product Other) {
+            return name.ToUpper().CompareTo(Other.name.ToUpper());
+        }
+    }
+}
---
+++ Exercicio_Linq/Exercicio_Linq/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exercicio_Linq.Entities;
+using System.IO;
+using System.Linq;
+
+Console.Write("Path of archive: ");
+string path = Console.ReadLine();
+
+List<Product> list = new List<Product>();
+
+try {
+    using (StreamReader sr = File.OpenText(path)) {
+        while (!sr.EndOfStream) {
+            string line = sr.ReadLine();
+            string[] separator = line.Split(',');
+            list.Add(new Product(separator[0], double.Parse(separator[1])));
+        }
+    }
+} catch (IOException error) {
+    Console.WriteLine(error.Message);
+}
+
+//Average
+double average = list.Average(p => p.price);
+Console.WriteLine("Average Price:" + average.ToString("F2"));
+
+list.Sort();
+//OU
+list.Sort((p1, p2) => p1.name.ToUpper().CompareTo(p2.name.ToUpper()));
+
+list.Reverse();
+foreach (var item in list) {
+    if(item.price < average) Console.WriteLine(item);
+}
+
+
+//apresentar a lista em ordem alfabética de forma decrescente
